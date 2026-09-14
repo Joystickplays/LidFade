33ef226ff8947245637758ec9d32373da6fc749f
Joystickplays <joystickgaming09@gmail.com>
2026-09-14 08:13:38 +0700

    Initial Commit

---
+++ LidFade/LidFadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidFadeApp.swift
+//  LidFade
+//
+//  Created by zabruh on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct LidFadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
